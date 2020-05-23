d6b6d8883de91e87b5873b4ce3ddb4968fd1d250
Michal Rozenek <rozenekmichal@gmail.com>
2020-05-23 14:27:58 +0200

    Modal done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Modal } from './components/Modal/Modal.component';
+import logo from './images/logo.svg';
 
 import './App.css';
 
-export const  App = () => (
-  <div className="App">
-    APP
-  </div>
-);
+export const  App = () => {
+  const [isModalOpen, setModalState] = React.useState(false);
+
+  const toggleModal = () => setModalState(!isModalOpen);
+
+  return (
+    <div className={'app'}>
+      <img
+        className={'app__logo'}
+        src={logo}
+        alt={'logo'}
+      />
+      <button
+        className={'app__btn'}
+        onClick={toggleModal}
+      >
+        Start now!
+      </button>
+      <Modal
+        title={'This is my modal'}
+        isOpen={isModalOpen}
+        onClose={toggleModal}
+      >
+        This is an example modal content!
+        Watch whole video to see how to code it!
+      </Modal>
+    </div>
+  );
+};
---
+++ src/components/Modal/Modal.component.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import iconX from '../../images/x.svg';
+
+import './Modal.component.css';
+
+interface ModalProps {
+  title: string;
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+export const Modal: React.FC<ModalProps> = ({ title, isOpen, onClose, children }) => {
+  const outsideRef = React.useRef(null);
+
+  const handleCloseOnOverlay = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    if (e.target === outsideRef.current) {
+      onClose();
+    }
+  }
+
+  return isOpen ? (
+    <div className={'modal'}>
+      <div
+        ref={outsideRef}
+        className={'modal__overlay'}
+        onClick={handleCloseOnOverlay}
+      />
+      <div className={'modal__box'}>
+        <button
+          className={'modal__close'}
+          onClick={onClose}
+        >
+          <img src={iconX} alt={'close'} />
+        </button>
+        <div className={'modal__title'}>
+          {title}
+        </div>
+        <div className={'modal__content'}>
+          { children }
+        </div>
+      </div>
+    </div>
+  ) : null;
+};
