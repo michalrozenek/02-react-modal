e037ffe6af8f6a2aaba8d58d7e2a9ced62c0ebaa
Michal Rozenek <rozenekmichal@gmail.com>
2020-05-30 20:55:06 +0200

    imp(Animation): Fade out animation added

--- src/components/Modal/Modal.component.tsx
+++ src/components/Modal/Modal.component.tsx
@@ -9,17 +9,41 @@ interface ModalProps {
   onClose: () => void;
 }
 
+enum ModalAnimation {
+  FADE_IN = 'fade-in',
+  FADE_OUT = 'fade-out',
+}
+
+const animationDelay = 500;
+let animationTimeout: NodeJS.Timeout | undefined = undefined;
+
 export const Modal: React.FC<ModalProps> = ({ title, isOpen, onClose, children }) => {
+  const [animationClass, setAnimationClass] = React.useState<ModalAnimation>(ModalAnimation.FADE_IN);
   const outsideRef = React.useRef(null);
+
+  const handleClose = () => {
+    setAnimationClass(ModalAnimation.FADE_OUT);
+    window.setTimeout(() => {
+      onClose();
+    }, animationDelay)
+  }
 
   const handleCloseOnOverlay = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
     if (e.target === outsideRef.current) {
-      onClose();
+      handleClose();
     }
   }
 
+  React.useEffect(() => {
+    isOpen && setAnimationClass(ModalAnimation.FADE_IN);
+  }, [isOpen])
+
+  React.useEffect(() => {
+    return () => animationTimeout && clearTimeout(animationTimeout);
+  })
+
   return isOpen ? (
-    <div className={'modal'}>
+    <div className={`modal modal--${animationClass}`}>
       <div
         ref={outsideRef}
         className={'modal__overlay'}
@@ -28,7 +52,7 @@ export const Modal: React.FC<ModalProps> = ({ title, isOpen, onClose, children }
       <div className={'modal__box'}>
         <button
           className={'modal__close'}
-          onClick={onClose}
+          onClick={handleClose}
         >
           <img src={iconX} alt={'close'} />
         </button>
